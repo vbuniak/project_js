d6e8cd9adc739678cf829c80ca93f6168e3094d6
Vitalii <vbuniak10@gmail.com>
2023-12-31 12:09:45 +0200

    First js task and css file

--- js/dist/script.dev.js
+++ js/dist/script.dev.js
@@ -21,3 +21,19 @@
 
 Проверить, чтобы все работало без ошибок в консоли */
 'use strict';
+
+var numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+var personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false
+};
+var a = prompt('Один из последних просмотренных фильмов?', ''),
+    b = prompt('На сколько оцените его?', ''),
+    c = prompt('Один из последних просмотренных фильмов?', ''),
+    d = prompt('На сколько оцените его?', '');
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+console.log(personalMovieDB);
