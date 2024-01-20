51b454010c2ffb84fae6a88bed9821ebbb443c79
Vitalii <vbuniak10@gmail.com>
2024-01-20 14:20:51 +0200

    new js file

--- js/dist/script.dev.js
+++ js/dist/script.dev.js
@@ -1,95 +1,32 @@
-/*Задание на урок:
-1) Создать переменную numberOfFilms и в неё поместить ответ от пользователя на вопрос:
-'Сколько фильмов вы уже посмотрели?'
-2) Создать объект personalMovieDB и в него поместить такие свойства:
-   - count - сюда передается ответ на первый вопрос
-   - movies - в это свойство поместить пустой объект
-   - actors - тоже поместить пустой объект
-   - genres - сюда поместить пустой массив
-   - privat - в это свойство поместить boolean(логическое) значение false
-3) Задайте пользователю по два раза вопросы:
-   - 'Один из последних просмотренных фильмов?'
-   - 'На сколько оцените его?'
-Ответы стоит поместить в отдельные переменные
-Записать ответы в объект movies в формате: 
-   movies: {
-       'logan': '8.1'
-   }
-Проверить, чтобы все работало без ошибок в консоли */
+/* Задание на урок:
+1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
+2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
+отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит - 
+возвращаем пользователя к вопросам опять
+3) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
+"Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше - 
+"Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
+4) Потренироваться и переписать цикл еще двумя способами*/
 'use strict';
-/*const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
+var numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+var personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('На сколько оцените его?', '');
+for (var i = 0; i < 2; i++) {
+  var a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
-
-console.log(personalMovieDB); */
-
-/*let hamburger;
-const fries = NaN;
-const cola = 0;
-const nuggets = 2;
-
-
-if (hamburger || cola || fries === 3 || nuggets) {
-   console.log('Done!')
-}
-
-
-console.log( 5 === 5 && 3 > 1 || 5);*/
-
-for (var i = 0; i < 3; i++) {
-  console.log(i);
-
-  for (var j = 0; j < 3; j++) {
-    console.log(j);
-  }
-} //*
-//**
-//***
-//****
-//*****
-//******
-
-
-var result = '';
-var lenth = 7;
-
-for (var _i = 1; _i < lenth; _i++) {
-  for (var _j = 0; _j < _i; _j++) {
-    result += '*';
-  }
-
-  result += '\n';
-}
-
-console.log(result);
-
-function firstTask() {
-  for (var _i2 = 5; _i2 < 11; _i2++) {
-    console.log(_i2);
+  if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+    personalMovieDB.movies[a] = b;
+  } else {
+    i--;
   }
 }
 
-firstTask();
-
-function secondTask() {
-  for (var _i3 = 20; _i3 >= 10; _i3--) {
-    if (_i3 === 13) break;
-    console.log(_i3);
-  }
-}
-
-secondTask();
+console.log(personalMovieDB);
--- js/script.js
+++ js/script.js
@@ -1,29 +1,21 @@
- /*Задание на урок:
+ /* Задание на урок:
 
-1) Создать переменную numberOfFilms и в неё поместить ответ от пользователя на вопрос:
-'Сколько фильмов вы уже посмотрели?'
+1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
 
-2) Создать объект personalMovieDB и в него поместить такие свойства:
-    - count - сюда передается ответ на первый вопрос
-    - movies - в это свойство поместить пустой объект
-    - actors - тоже поместить пустой объект
-    - genres - сюда поместить пустой массив
-    - privat - в это свойство поместить boolean(логическое) значение false
+2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
+отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит - 
+возвращаем пользователя к вопросам опять
 
-3) Задайте пользователю по два раза вопросы:
-    - 'Один из последних просмотренных фильмов?'
-    - 'На сколько оцените его?'
-Ответы стоит поместить в отдельные переменные
-Записать ответы в объект movies в формате: 
-    movies: {
-        'logan': '8.1'
-    }
+3) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
+"Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше - 
+"Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
 
-Проверить, чтобы все работало без ошибок в консоли */
+4) Потренироваться и переписать цикл еще двумя способами*/
+
 
 'use strict';
 
-/*const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -33,109 +25,15 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('На сколько оцените его?', '');
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+          b = prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
-
-console.log(personalMovieDB); */
-
-/*let hamburger;
-const fries = NaN;
-const cola = 0;
-const nuggets = 2;
-
-
-if (hamburger || cola || fries === 3 || nuggets) {
-   console.log('Done!')
-}
-
-
-console.log( 5 === 5 && 3 > 1 || 5);*/
-
-
-for (let i = 0; i < 3; i++) {
-    console.log(i);
-    for (let j = 0; j < 3; j++) {
-        console.log(j);
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+    } else {
+        i--;
     }
 }
+console.log(personalMovieDB); 
 
-//*
-//**
-//***
-//****
-//*****
-//******
-
-function firstTask() {
-    for (let i = 5; i < 11; i++) {
-        console.log(i);
-    }
-}
-
-firstTask()
-
-function secondTask() {
-    for(let i = 20; i >= 10; i--) {
-        if (i === 13) break;
-        console.log(i)
-    }
-}
-
-secondTask()
-
-function thirdTask() {
-    for(let i = 2; i <= 10; i++) {
-        if(i % 2 === 0) {
-            console.log(i);
-        }
-    }
-}
-
-thirdTask()
-
-function fifthTask() {
-    const arrayOfNumbers = [];
-
-    for (let i = 5; i < 11; i++) {
-        arrayOfNumbers[i - 5] = i;
-    }
-
-    console.log(arrayOfNumbers);
-    return arrayOfNumbers;
-}
-
-fifthTask()
-
-function firstTask() {
-    const arr = [3, 5, 8, 16, 20, 23, 50];
-    const result = [];
-
-    for (let i = 0; i < arr.length; i++) {
-        result[i] = arr[i];
-    }
-
-    console.log(result);
-    return result;
-}
-
-firstTask();
-
-const lines = 5;
-let result = '';
-
-for (let i = 0; i <= lines; i++) {
-    for (let j = 0; j < lines - i; j++) {
-        result += " ";
-    }
-    for (let j = 0; j < 2 * i + 1; j++) {
-        result += "*";
-    }
-    result += "\n";
-}
-
-console.log(result)
